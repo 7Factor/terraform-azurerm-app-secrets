6bb2bff7cddd86760c28c00a274f64135b747249
Greg Willard <gwillard@r3pwn.com>
2026-01-28 12:45:41 -0500

    initial commit of app_secrets module

---
+++ kv.tf
@@ -0,0 +1,52 @@
+locals {
+  create_kv  = length(var.app_secrets) > 0
+  
+  key_vault = try(data.azurerm_key_vault.web_app[0], azurerm_key_vault.web_app[0], null)
+
+  needs_kv_role = length(local.app_secret_bindings) > 0
+}
+
+data "azurerm_resource_group" "existing_rg" {
+  name = var.key_vault_settings.rg_name
+}
+
+resource "azurerm_key_vault" "web_app" {
+  count = local.create_kv && !var.key_vault_settings.externally_created ? 1 : 0
+
+  name                = var.key_vault_settings.name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = var.key_vault.sku
+
+  rbac_authorization_enabled = true
+  purge_protection_enabled   = var.key_vault.purge_protection_enabled
+  soft_delete_retention_days = var.key_vault.soft_delete_retention_days
+}
+
+resource "azurerm_role_assignment" "webapp_kv_reader" {
+  count = local.needs_kv_role ? 1 : 0
+
+  scope                = local.key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.web_app[0].principal_id
+}
+
+data "azurerm_key_vault" "web_app" {
+  count = var.key_vault_settings.externally_created ? 1 : 0
+
+  name                = var.key_vault_settings.name
+  resource_group_name = var.key_vault_settings.rg_name
+}
+
+resource "azurerm_key_vault_secret" "linked" {
+  for_each = local.app_secrets_by_name
+
+  name         = each.key
+  value        = each.value.initial_value != null ? each.value.initial_value : ""
+  key_vault_id = local.key_vault.id
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "app_secrets" {
+  description = "List of secrets to create and optionally create app settings bindings for."
+  type = list(object({
+    name             = string
+    app_setting_name = optional(string)
+    initial_value    = optional(string)
+  }))
+  default   = []
+  sensitive = true
+
+  validation {
+    condition     = length([for s in var.app_secrets : s.name]) == length(distinct([for s in var.app_secrets : s.name]))
+    error_message = "Each app_secrets entry must have a unique 'name'."
+  }
+}
+
+locals {
+  app_secrets_by_name = {
+    for s in nonsensitive(var.app_secrets) : s.name => sensitive(s)
+  }
+  app_secret_bindings = {
+    for s in nonsensitive(var.app_secrets) : s.app_setting_name => s.name
+    if s.app_setting_name != null && length(s.app_setting_name) > 0
+  }
+}
+
+variable "key_vault_settings" {
+  description = "Key Vault settings. `name` and `rg_name` are required"
+  type = object({
+    name                       = string
+    rg_name                    = string
+    externally_created         = optional(bool, false)
+    sku                        = optional(string, "standard")
+    purge_protection_enabled   = optional(bool, false)
+    soft_delete_retention_days = optional(number, 7)
+  })
+  default = {}
+}
+
+variable "managed_identity_principal_id" {
+  description = "(Optional) Principal ID of the managed identity to assign Key Vault permissions to. If you already handle this permission elsewhere, you may omit this value."
+  type        = string
+  default     = null
+}
