2aafbfc13841d98d332b2661ac3931aa1b45a3ec
Greg Willard <gwillard@r3pwn.com>
2026-02-02 17:23:48 -0500

    expose keyvault references for usage in connection strings, etc

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
+locals {
+  key_vault_references = {
+    for secret in nonsensitive(var.app_secrets) :
+    secret.name => "@Microsoft.KeyVault(SecretUri=${local.key_vault.vault_uri}secrets/${secret.name}/)"
+  }
+}
+
 output "key_vault" {
   value = local.key_vault
 }
@@ -5,6 +12,10 @@ output "key_vault" {
 output "app_settings_bindings" {
   value = length(local.app_secret_bindings) > 0 ? {
     for app_setting_key, secret_name in local.app_secret_bindings :
-    app_setting_key => "@Microsoft.KeyVault(SecretUri=${local.key_vault.vault_uri}secrets/${secret_name}/)"
+    app_setting_key => local.key_vault_references[secret_name]
   } : {}
+}
+
+output "key_vault_references" {
+  value = local.key_vault_references
 }
